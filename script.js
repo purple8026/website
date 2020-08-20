db5b4e8bf268ffa07eaa10652073e72a05023056
purple8026 <68952112+purple8026@users.noreply.github.com>
2020-08-20 10:11:25 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function buttonFunction() {
+var name = prompt("What is your name?");
+				console.log(name);
+var address = prompt("What is your address?");
+				console.log(address);
+var number = prompt("Enter your card number:");
+				console.log(number);
+var expiry = prompt("Enter the expiry date:");
+     console.log(expiry);
+var cvc = prompt("Enter the CVC:");
+	    console.log(cvc);
+				}
+var	list	= [];
+
+				
+					
+
+
